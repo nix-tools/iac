9de60437f81b00b3c6250ba54dcaffd7be4c8449
Simon Shine <simon@simonshine.dk>
2025-04-25 16:37:02 +0200

    doc(projects): Add commented-out nix-roadmaps project
    GitHub provider does not currently support project creation.

---
+++ projects.tf
@@ -0,0 +1,5 @@
+# HTTP 410 Gone: GitHub provider does not currently support project creation.
+#resource "github_organization_project" "nix_roadmaps_project" {
+#  name = "nix-roadmaps"
+#  body = "Coordinating the nix-roadmaps project"
+#}
